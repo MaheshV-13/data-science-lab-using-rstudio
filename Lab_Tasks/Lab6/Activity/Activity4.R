344aaa114a9cf6e14481cb305187b6aaa3e2b3ca
MaheshV-13 <maheshvigneswaran13@gmail.com>
2026-02-23 11:04:32 +0800

    update

---
+++ Lab_Tasks/Lab6/Activity/Activity4.R
@@ -0,0 +1,28 @@
+student.data <- data.frame(
+  name = c("Anastasia", "Dima", "Michael", "Matthew", "Laura", "Kevin", "Jonas"),
+  score = c(12.5, 9.0, 16.5, 12.0, 9.0, 8.0, 19.0),
+  attempts = c(1, 3, 2, 3, 2, 1, 2)
+)
+
+student.data$qualify <- c("yes", "no", "yes", "no", "no", "no", "yes")
+
+student.newdata <- data.frame(
+  name = c("Emily"),
+  score = c(14.5),
+  attempts = c(1),
+  qualify = c("yes")
+)
+
+student.finaldata <- rbind(student.data, student.newdata)
+
+str(student.finaldata)
+
+#changing qualify data type to factor to show the amount of yes and no
+student.finaldata$qualify <- as.factor(student.finaldata$qualify)
+
+print(summary(student.finaldata))
+print(dim(student.finaldata))
+print(ncol(student.finaldata))
+print(nrow(student.finaldata))
+
+
